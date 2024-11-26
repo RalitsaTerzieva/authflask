1965419ff8667f83746f28c6bfc345e504c8fedd
Ralica Popova <ralicaterzieva90@gmail.com>
2024-11-26 18:21:12 +0200

    Fix error to make the app run again

--- app.py
+++ app.py
@@ -20,28 +20,29 @@ def logout():
     logout_user()
     flash('You logged out!')
     return redirect(url_for('home'))
-
 @app.route('/login', methods=["POST", "GET"])
 def login():
-
     form = LoginForm()
 
     if form.validate_on_submit():
-
         user = User.query.filter_by(email=form.email.data).first()
 
-        if user.check_password(form.password.data) and user is not None:
+        if user and user.check_password(form.password.data):
             login_user(user)
             flash("Logged in Successfully!")
 
             next = request.args.get('next')
 
-            if next is not None or not next[0] == '/':
+            if next is None or next[0] != '/':
                 next = url_for('welcome_user')
 
             return redirect(next)
         
+        flash("Login Unsuccessful. Please check email and password.")
         return render_template('login.html', form=form)
+
+    return render_template('login.html', form=form)
+
     
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -50,11 +51,18 @@ def register():
 
     if form.validate_on_submit():
 
-        user = User.query.get(email=form.email.data, username=form.username.data, password=form.password.data)
+        user = User.query.filter_by(email=form.email.data).first()
 
-        db.session.add(user)
+        if user:
+            flash('Email already registered!')
+            return redirect(url_for('register'))
+        
+        new_user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+
+        db.session.add(new_user)
         db.session.commit()
         flash('Thanks for registration!')
+
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
