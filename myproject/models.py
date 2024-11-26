fb12a7884641219f8b920a1f72e4e9d3e7c3f4da
Ralica Popova <ralicaterzieva90@gmail.com>
2024-11-26 15:11:13 +0200

    Add load user function

--- myproject/models.py
+++ myproject/models.py
@@ -2,6 +2,11 @@ from myproject import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+@login_manager.user_loader #buildin decorator from the login manager
+def load_user(user_id):
+    return db.session.get(User, user_id)
+
+
 class User(db.Model, UserMixin):
 
     __tablename_ = 'users'
