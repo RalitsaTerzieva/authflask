99ad8c751ca8f25853b071c830ed46200078b43b
Ralica Popova <ralicaterzieva90@gmail.com>
2024-11-26 16:33:24 +0200

    Add login view logic

--- app.py
+++ app.py
@@ -13,3 +13,33 @@ def home():
 @login_required
 def welcome_user():
     return render_template('welcome_user.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You logged out!')
+    return redirect(url_for('home'))
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user.check_password(form.password.data) and user is not None:
+            login_user(user)
+            flash("Logged in Successfully!")
+
+            next = request.args.get('next')
+
+            if next is not None or not next[0] == '/':
+                next = url_for('welcome_user')
+
+            return redirect(next)
+        
+        return render_template('login.html', form=form)
+
